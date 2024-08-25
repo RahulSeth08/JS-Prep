78d4bab5f695edfb94a837248e99809581ea897b
Rahul Seth <119778639+RahulSeth08@users.noreply.github.com>
2024-08-25 19:55:32 +0000

    Initial Commit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function add(){
-  return a+b;
+
+// function chage() {
+//   const name = document.getElementById("college");
+//   console.log(name)
+//   name.innerHTML = name.innerHTML.trim() == "Galgotias College Of Engineering & Technology" ? "IIT Delhi" : "Galgotias College Of Engineering & Technology";
+// }
+
+
+function change() {
+  const name = document.getElementById("college")
+  console.log(name)
+  name.innerHTML = name.innerHTML == "IIT Delhi" ? "Galgotias College Of Engineering & Technology" : "IIT Delhi"
+
 }
+
+
